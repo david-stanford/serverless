0c1094d9ed68eca44904ede1eddc1cc0b062a6d3
David Stanford <dastanfo@microsoft.com>
2018-04-18 17:35:10 -0700

    initial commit for serverless web app

---
+++ js/uploader.js
@@ -0,0 +1,51 @@
+var fileName;
+
+function startRead(evt) {
+    var file = document.getElementById('file').files[0];
+    if (file) {
+        if (file.type.match("image.*")) {
+            getAsImage(file, addImg);
+            fileName = file.name;
+            alert("Name: " + file.name + "\n" + "Last Modified Date :" + file.lastModifiedDate);
+        }
+    }
+}
+
+function getAsImage(readFile, callback) {
+    var reader = new FileReader();
+    reader.readAsDataURL(readFile);
+    reader.onload = callback;
+}
+
+function addImg(imgsrc) {
+    var imageFile = document.createElement('img');
+    imageFile.setAttribute("src", imgsrc.target.result);
+    document.getElementById("preview").insertBefore(imageFile, null);
+}
+
+function cleanupAndUpload(imageData){
+    var cleanData = imageData.target.result.split("base64,").pop();
+    var myData = new FormData();
+    myData.append("image", cleanData);
+    var request = new XMLHttpRequest();
+    var url = 'https://triplecrown.azurewebsites.net/api/image/put/' + fileName;
+    request.open("POST", url, true);
+    request.send(myData);
+}
+
+function upload(){
+
+    // Loop through each of the selected files.
+    var fileSelect = document.getElementById('file');
+    var files = fileSelect.files;
+    for (var i = 0; i < files.length; i++) {
+        var file = files[i];
+        getAsImage(file,cleanupAndUpload);
+        // Check the file type.
+        if (!file.type.match('image.*')) {
+            continue;
+        }
+    }
+//https://stackoverflow.com/questions/166221/how-can-i-upload-files-asynchronously
+//http://blog.teamtreehouse.com/uploading-files-ajax
+}
